f646b3a56d4a5e20019cbb8f4bfcf1d46e85afa8
滑川裕里瑛 <lucky854854@gmail.com>
2023-08-11 23:26:17 +0900

    戻るボタンつけたなどなど

--- friendmap/MainroomViewController.swift
+++ friendmap/MainroomViewController.swift
@@ -28,6 +28,8 @@ class MainroomViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
                 let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
                 self.view.addSubview(mapView)
@@ -38,6 +40,9 @@ class MainroomViewController: UIViewController {
                 marker.title = "Sydney"
                 marker.snippet = "Australia"
                 marker.map = mapView
+        
+        view.addSubview(mapView)
+        view.sendSubviewToBack(mapView)
         
 //        view.addSubview(mapView)
 
